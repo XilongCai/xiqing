93c5475ad75dc63ae95ada39d6cf9bd0432e087c
蔡希隆 <caixilong@sainozomitakashis-MacBook-Air.local>
2024-03-19 01:36:01 -0700

    change url

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://127.0.0.1:5000/generate_plot', {
+      const response = await fetch('https://xiqing-back-1.onrender.com/generate_plot', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

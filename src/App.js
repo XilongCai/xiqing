39223d98032f330b5a9571e60b1ebdb965ad31c8
蔡希隆 <caixilong@sainozomitakashis-MacBook-Air.local>
2024-03-21 02:14:20 -0700

    add release

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   const [x, setX] = useState('');
   const [y, setY] = useState('');
   const [imageSrc, setImageSrc] = useState('');
+  const [currentPage, setCurrentPage] = useState('generatePlot');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,9 +24,31 @@ const App = () => {
     }
   };
 
+  const handleSubmitRelease = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('https://xiqing-back-1.onrender.com/generate_plot_release', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ x, y }),
+      });
+      const data = await response.json();
+      setImageSrc(`data:image/png;base64,${data.plotBase64}`);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
+    
     <div className="container">
-      <h1>Hydrogen Absorption Model Plot</h1>
+      <nav className="navbar">
+        <button className={currentPage === 'generatePlot' ? 'active' : ''} onClick={() => {setCurrentPage('generatePlot'); setX(''); setY(''); setImageSrc('');}}>Absorption</button>
+        <button className={currentPage === 'generatePlotRelease' ? 'active' : ''} onClick={() => {setCurrentPage('generatePlotRelease'); setX(''); setY(''); setImageSrc('');}}>Release</button>
+      </nav>
+      {currentPage === 'generatePlot' ? (<><h1>Hydrogen Absorption Model Plot</h1>
       <form onSubmit={handleSubmit}>
         <div className="input-group">
           <label htmlFor="x">Enter Velocity Value:</label>
@@ -49,7 +72,31 @@ const App = () => {
         </div>
         <button type="submit">Generate Plot</button>
       </form>
-      <div>{imageSrc && <img src={imageSrc} alt="Plot" className="plot-image" />}</div>
+      <div>{imageSrc && <img src={imageSrc} alt="Plot" className="plot-image" />}</div></>) : <><h1>Hydrogen Release Model Plot</h1>
+      <form onSubmit={handleSubmitRelease}>
+        <div className="input-group">
+          <label htmlFor="x">Enter Velocity Value:</label>
+          <input
+            type="text"
+            id="x"
+            value={x}
+            onChange={(e) => setX(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="y">Enter Temperature Value:</label>
+          <input
+            type="text"
+            id="y"
+            value={y}
+            onChange={(e) => setY(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Generate Plot</button>
+      </form>
+      <div>{imageSrc && <img src={imageSrc} alt="Plot" className="plot-image" />}</div></>}
     </div>
   );
 };
